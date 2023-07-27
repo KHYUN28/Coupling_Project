224ad03704c2cf52f6262d90c68f141fbd60a98c
KKHYUN <121412134+KHYUN28@users.noreply.github.com>
2023-07-27 16:12:33 +0900

    Update MetadataCollector.java

--- src/com/kkh/collector/MetadataCollector.java
+++ src/com/kkh/collector/MetadataCollector.java
@@ -2,7 +2,7 @@ package com.kkh.collector;
 
 import com.kkh.export.ExportMetadata;
 import com.kkh.fetch.FetchMetadata;
-
+// 주석넣기
 public interface MetadataCollector {
 	public void setMetadataCollector(FetchMetadata fetchMetadata, ExportMetadata exportMetadata);
 	public void collectMetadata();
